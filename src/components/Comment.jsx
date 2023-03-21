378cf75103e0b631ded2439ff44a1f263a854845
marcelopajr <marcelo96jr@gmail.com>
2023-03-21 15:57:49 -0300

    chore: application structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,41 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar src="https://github.com/marcelopajr.png" hasBorder={false} />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Marcelo Pereira</strong>
+              <time
+                title="11 de Janeiro às 08:13h"
+                dateTime="2023-01-11 08:13:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilLine } from "phosphor-react";
+import userCover from "../assets/user-cover.png";
+import { Avatar } from "./Avatar";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src={userCover} />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/marcelopajr.png" />
+
+        <strong>Marcelo Pereira</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
