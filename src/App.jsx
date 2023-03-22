279b9d82e996a6d2043d4b189444ae3e690d69bb
marcelopajr <marcelo96jr@gmail.com>
2023-03-22 15:43:05 -0300

    chore: add post props and date formatter

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,45 @@ import { Post } from "./components/Post";
 import "./global.css";
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/marcelopajr.png",
+      name: "Marcelo Pereira",
+      role: "Front-end Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-13 20:00:00"),
+  },
+];
+
 function App() {
   return (
     <>
@@ -14,7 +53,15 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                publishedAt={post.publishedAt}
+                content={post.content}
+              />
+            );
+          })}
         </main>
       </div>
     </>
