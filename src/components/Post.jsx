9180220f07c4369aa6e03b69d6f0e31559badabd
marcelopajr <marcelo96jr@gmail.com>
2023-03-24 17:05:38 -0300

    feat: add validate form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,6 +32,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText("");
   }
 
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório!");
+  }
+
   function deleteComment(commentToDelete) {
     const commentsWithoutDeletedOne = comments.filter((comment) => {
       return comment !== commentToDelete;
@@ -39,6 +43,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -82,10 +88,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
