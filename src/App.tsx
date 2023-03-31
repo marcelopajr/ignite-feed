874dddfe35a4225a08d861f69c6c38a26597b4b5
marcelopajr <marcelo96jr@gmail.com>
2023-03-31 18:42:02 -0300

    chore: add interface extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 
 import "./global.css";
 import styles from "./App.module.css";
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -54,14 +54,7 @@ function App() {
 
         <main>
           {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                publishedAt={post.publishedAt}
-                content={post.content}
-              />
-            );
+            return <Post key={post.id} post={post} />;
           })}
         </main>
       </div>
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,15 +1,15 @@
-import styles from "./Avatar.module.css";
+import { ImgHTMLAttributes } from "react";
 
-interface AvatarProps {
-  src: string;
+import styles from "./Avatar.module.css";
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
 }
 
-export function Avatar({ src, hasBorder = true }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
+      {...props}
     />
   );
 }
